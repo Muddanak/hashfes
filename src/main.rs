d14db319e0c826f89b8ad7530e088631372b8062
Steve <MuddobberSK@gmail.com>
2023-04-12 14:51:05 -0500

    Added comments for what is happening when

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,29 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
+    //Check that the user actually has a filename provided
     if args.filename != "" {
+        //Open the file for the BufReader to pull data from
         let mut reader = BufReader::new(File::open(args.filename)?);
+
+        //Create the buffer
         let mut buffer = Vec::new();
 
+        //Read the entire file into the buffer
         let bufsize = reader.read_to_end(&mut buffer).unwrap();
+
+        //Print out how many lines were read
         println!("Read {} bytes", bufsize);
 
+        //Create the hashers for SHA-256 and MD5
         let mut sha256hasher = Sha256::new();
         let mut md5hasher = Md5::new();
 
+        //Set the data and then digest for the MD5
         md5hasher.update(&buffer);
         println!("MD5:\t{:X}", md5hasher.finalize());
 
+        //Set the data and then digest for SHA-256
         sha256hasher.update(&buffer);
         println!("SHA256:\t{:X}", sha256hasher.finalize());
 
